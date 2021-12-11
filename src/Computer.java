3fd00ee668af8f55fc256cb82168869332bf199e
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2021-12-11 12:22:01 +0500

    add new class

---
+++ src/Computer.java
@@ -0,0 +1,20 @@
+public class Computer {
+    private int memory;
+    private String videoCard;
+
+    public int getMemory() {
+        return memory;
+    }
+
+    public void setMemory(int memory) {
+        this.memory = memory;
+    }
+
+    public String getVideoCard() {
+        return videoCard;
+    }
+
+    public void setVideoCard(String videoCard) {
+        this.videoCard = videoCard;
+    }
+}
